6289970939376063427d04f082f1bc8daa40248c
eliasfelipedasilva1999 <eliasfelipedasilva1999@gmail.com>
2020-01-24 15:01:20 +0000

    Responsividade... Popover

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -60,6 +60,34 @@ sap.ui.define([
 				this.getRouter().navTo("worklist", {}, true);
 			}
 		},
+		
+		/**
+		 * Event handler for press event on object identifier. 
+		 * opens detail popup from component to show product dimensions.
+		 * @public
+		 */
+			onShowDetailPopover : function (oEvent) {
+		
+			var oPopover = this._getPopover();
+			var oSource = oEvent.getSource();
+			oPopover.bindElement(oSource.getBindingContext().getPath());
+		
+			// open dialog
+			oPopover.openBy(oEvent.getParameter("domRef"));
+		},
+		
+		_getPopover : function () {
+			// create dialog lazily
+				if (!this._oPopover) {
+					// create popover via fragment factory
+					this._oPopover = sap.ui.xmlfragment(
+					"opensap.manageproducts.view.ResponsivePopover", this);
+					this.getView().addDependent(this._oPopover);
+				}
+				return this._oPopover;
+			},
+
+
 
 		/* =========================================================== */
 		/* internal methods                                            */
